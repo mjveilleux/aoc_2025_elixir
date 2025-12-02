caf8030330d1aa5d0042edc1a44bd8ccc624dc84
Mason Veilleux <masonjveilleux@gmail.com>
2025-12-02 10:49:53 -0900

    completed part 1 of 2nd challenge

--- lib/aoc/2_giftshop/solve.ex
+++ lib/aoc/2_giftshop/solve.ex
@@ -4,19 +4,22 @@ defmodule Solve.ResolveInvalidIDs do
 
   def run do
     invalid_ids =
-      for c <- Helpers.get_chunks() do
-        Logger.debug("Loaded #{c}")
-        {a, b} = Helpers.parse_for_ab(c)
-        raw_list = Helpers.ab_range(a, b)
-        list_of_candidates = Helpers.find_potential_candidates(raw_list)
+      Helpers.get_chunks()
+      |> Task.async_stream(
+        fn c ->
+          Logger.debug("Loaded #{c}")
+          {a, b} = Helpers.parse_for_ab(c)
+          raw_list = Helpers.ab_range(a, b)
+          list_of_candidates = Helpers.find_potential_candidates(raw_list)
 
-        list_of_candidates
-        |> Enum.map(&Helpers.evaluate_invalid_ids/1)
-        |> Enum.reject(&is_nil/1)
-      end
-      |> Enum.flat_map(& &1)
+          list_of_candidates
+          |> Enum.map(&Helpers.evaluate_invalid_ids/1)
+          |> Enum.reject(&is_nil/1)
+        end,
+        timeout: :infinity
+      )
+      |> Enum.flat_map(fn {:ok, result} -> result end)
 
-    Logger.debug("#{invalid_ids}")
     json = Jason.encode!(invalid_ids)
     File.write!("lib/aoc/2_giftshop/invalid_ids.json", json)
     %{count: length(invalid_ids), sum: Enum.sum(invalid_ids)}
